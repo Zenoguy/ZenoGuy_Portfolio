e5ea2ac760cc9ac5991fec683f47f6cbf1f157f6
zenoguy <ghoshshreyan04042005@gmail.com>
2025-12-20 19:36:11 +0530

    bug fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -269,7 +269,7 @@ function Philosophy() {
 /* HUMAN / HOBBIES                                                     */
 /* ------------------------------------------------------------------ */
 
-export  function HobbiesSection() {
+function HobbiesSection() {
   const hobbies = [
     { 
       icon: "/images/gym.svg", 
